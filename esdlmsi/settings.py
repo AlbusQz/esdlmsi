124b1f50edd2323f8b27a62f8f26e2eed11a0ac6
Albus-QZ <974352604@qq.com>
2023-07-16 16:11:19 +0800

    数据库model摸索提交

--- esdlmsi/settings.py
+++ esdlmsi/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -81,8 +82,13 @@ WSGI_APPLICATION = 'esdlmsi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'esdlmsi',
+        'USER':"test",
+        'PASSWORD':"12345678",
+        'HOST':"localhost",
+        'PORT':"3306",
+
     }
 }
 
